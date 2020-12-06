a7b41631870743294f2d81e7579cdc439f284791
Tomek <t.pasternak94@gmail.com>
2020-12-06 23:40:22 +0100

    feat (1): home view and url added, initial setup of base html + extension of base - home html

--- outer_project/outer_project/urls.py
+++ outer_project/outer_project/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from apps.core.views import home
 urlpatterns = [
+    # admin
     path('admin/', admin.site.urls),
+
+    # homepage
+    path('', home, name='home'),
 ]
