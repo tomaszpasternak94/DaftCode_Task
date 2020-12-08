4f6b559896af0a6237eaf55042cbe61c43d25057
Tomek <t.pasternak94@gmail.com>
2020-12-08 23:05:55 +0100

    feat (2): new crud app added to the project

--- outer_project/outer_project/settings.py
+++ outer_project/outer_project/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # core
     'apps.core',
+    # rest framework
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
